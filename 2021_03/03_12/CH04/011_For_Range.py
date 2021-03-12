d3868b6a670964923045162d0ec0bc755a97b240
ITlearning <yo7504@naver.com>
2021-03-12 14:19:44 +0000

    03월 12일 중간 커밋

---
+++ 2021_03/03_12/CH04/011_For_Range.py
@@ -0,0 +1,13 @@
+# for 반복문과 범위
+# for 반복문과 범위를 함께 조합해서 사용합니다
+for i in range(5) : # range(5) 라는 뜻은 0 부터 5 - 1 까지의 범위를 지정한다라는 뜻
+    print(str(i) + "= 반복 변수")
+print()
+
+for i in range(5, 10) : # range(5,10) 라는 뜻은 5 부터 10 -1 까지의 범위를 지정한다라는 뜻
+    print(str(i) + "= 반복 변수")
+print()
+
+for i in range(0,10,3) : # range(0,10,3) 라는 뜻은 0부터 10 - 1 까지 3 씩 떨어져서 나오는 범위를 지정한다라는 뜻
+    print(str(i) + "= 반복 변수")
+print()
