dc16141cb575e56f57fb1a3ffd1404d140f9a9a0
ITlearning <yo7504@naver.com>
2021-03-14 14:38:11 +0000

    03월 14일 README.md

--- Test_Code.py
+++ Test_Code.py
@@ -1,5 +1,8 @@
 # 테스트 페이지입니다.
 # 약간의 실습이나 문제가 생겼을 경우 사용합니다.
 
-for i in range(3,10,3) :
-    print(i)
+output = [num for num in range(1,101) if "{:b}".format(num).count('0') == 1 ]
+
+for i in output :
+    print("{} : {}".format(i, "{:b}".format(i)))
+print("합계: ",sum(output))
